cd188501cece8444e3d14dece954795e7179acbe
Sebastián Díaz <sebastian88@gmail.com>
2022-09-11 00:45:24 -0400

    add redux counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Button variant="contained">Hello World</Button>
+      <div className="App">
+      <header className="App-header">
+        <Counter />
+      </header>
+    </div>
     </React.Fragment>
   );
 }
